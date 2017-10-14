4de8ab7fa3ad4e1d1b547e97dbdbfbc0c74be9ef
KirillKudaev <kirill.a.kudaev@gmail.com>
2017-10-14 17:47:41 -0500

    Initial Commit

---
+++ PrinFoodPlus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrinFoodPlus
+//
+//  Created by Kirill Kudaev on 10/14/17.
+//  Copyright © 2017 Kirill Kudaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
